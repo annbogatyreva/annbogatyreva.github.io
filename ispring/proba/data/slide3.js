fefca22c688af1ba16552ad3d99d6279f427d9a1
annbogatyreva <107212248+annbogatyreva@users.noreply.github.com>
2022-11-17 21:18:12 +0700

    Add files via upload

---
+++ ispring/proba/data/slide3.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{877CFE35-20C9-4C7D-A6B9-5F1E139C98B4}'];loadHandler&&loadHandler(2, '<div id="spr0_75cf3d"><div id="spr1_75cf3d" class="kern slide"><img id="img0_75cf3d" src="data/img2.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_75cf3d" class="kern slide"></div></div>', '{"s":[]}');})();
