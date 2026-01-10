44ac76def227631e153c4652d42ca808982ec056
annbogatyreva <bogatanutka@gmail.com>
2026-01-10 11:33:19 +0700

    1

---
+++ test0126/leadership/story_content/user.js
@@ -0,0 +1,319 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script2 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script3 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script4 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script5 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script6 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script7 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script8 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script9 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script10 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script11 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script12 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script13 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script14 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script15 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script16 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script17 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script18 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script19 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script20 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script21 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script22 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script23 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script24 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script25 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script26 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script27 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script28 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script29 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+}
+
+window.Script30 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script31 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+}
+
+window.Script32 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+window.Script33 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+}
+
+window.Script34 = function()
+{
+  var player=GetPlayer(); 
+var currentscore=player.GetVar("total_score"); 
+lmsAPI.SetScore(currentscore,100,0); 
+lmsAPI.SetReachedEnd(); 
+lmsAPI.SetPassed(); 
+}
+
+};
