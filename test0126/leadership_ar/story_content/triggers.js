fbbf6078c52caabc3b8b9ebbeffeedc9faac9307
annbogatyreva <bogatanutka@gmail.com>
2026-01-10 12:07:14 +0700

    ar

---
+++ test0126/leadership_ar/story_content/triggers.js
@@ -0,0 +1,125 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "63H2mENy8x4":
+        Script1();
+        break;
+      case "6MGjY3YFNiU":
+        Script2();
+        break;
+      case "5oPCFjX6e7H":
+        Script3();
+        break;
+      case "5kdBRT45C3l":
+        Script4();
+        break;
+      case "6YwXakY0iNv":
+        Script5();
+        break;
+      case "6l7caDGnUM5":
+        Script6();
+        break;
+      case "6bqjvWaFEMU":
+        Script7();
+        break;
+      case "6PEhUrJkbHM":
+        Script8();
+        break;
+      case "6PFZAQcg2K3":
+        Script9();
+        break;
+      case "5eFM6vfg1r0":
+        Script10();
+        break;
+      case "6ZTfKl3KudA":
+        Script11();
+        break;
+      case "5mwTMXGOASU":
+        Script12();
+        break;
+      case "64pQV8a1DOv":
+        Script13();
+        break;
+      case "6axNonVRuxK":
+        Script14();
+        break;
+      case "5yAMqrinzDV":
+        Script15();
+        break;
+      case "6qglf5tZeNF":
+        Script16();
+        break;
+      case "5WLu7X5xNcy":
+        Script17();
+        break;
+      case "65QYLsSP917":
+        Script18();
+        break;
+      case "6CZ02W9HBwL":
+        Script19();
+        break;
+      case "5uirLQt2Ada":
+        Script20();
+        break;
+      case "6R0BKABxA8f":
+        Script21();
+        break;
+      case "6kNU95H1PSC":
+        Script22();
+        break;
+      case "5yhPf8ov7WV":
+        Script23();
+        break;
+      case "6PEhJp8Ex0s":
+        Script24();
+        break;
+      case "6lQLAEbxSK9":
+        Script25();
+        break;
+      case "6ExjkvqoqL4":
+        Script26();
+        break;
+      case "5W8R81oV1KM":
+        Script27();
+        break;
+      case "68f8Ydqi92n":
+        Script28();
+        break;
+      case "6NDVkZfMri1":
+        Script29();
+        break;
+      case "6MN2jsAqLFZ":
+        Script30();
+        break;
+      case "5e7ADoTEOYP":
+        Script31();
+        break;
+      case "5kJFsaROuz6":
+        Script32();
+        break;
+      case "6RbQgP6qF0X":
+        Script33();
+        break;
+      case "6B4F2cP5pfc":
+        Script34();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
